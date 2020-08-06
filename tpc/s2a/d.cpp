0b98f69afe08d2b6df351dd2d753eff6c8d530c2
lennonyang <lennonyang@tencent.com>
2020-08-06 21:21:03 +0800

    update

--- tpc/s2a/d.cpp
+++ tpc/s2a/d.cpp
@@ -4,14 +4,37 @@ const int maxl = 1e5 + 10;
 
 struct node {
     int l, r;
-    char col;
+    char col[8];
     char flag;
-} nd[maxl * 8];
+} nd[maxl * 4];
 
 int t;
 int n, m;
 char s[maxl], ch[10];
 int op, l, r;
+char a[8] = {' ', 'r', 'y', 'b', 'o', 'p', 'g', 'x'};
+
+int getIdx(char ch) {
+    switch (ch) {
+        case ' ':
+            return 0;
+        case 'r':
+            return 1;
+        case 'y':
+            return 2;
+        case 'b':
+            return 3;
+        case 'o':
+            return 4;
+        case 'p':
+            return 5;
+        case 'g':
+            return 6;
+        case 'x':
+            return 7;
+    }
+    return 0;
+}
 
 char calc(int a, int b) {
     if (a == 'r' && b == 'y')
@@ -45,6 +68,13 @@ char calc(int a, int b) {
     return b;
 }
 
+void update(int now) {
+    for (int i = 0; i < 8; i++) {
+        int idx = getIdx(nd[now * 2].col[i]);
+        nd[now].col[i] = nd[now * 2 + 1].col[idx];
+    }
+}
+
 void build(int now, int l, int r) {
     nd[now].l = l;
     nd[now].r = r;
@@ -53,9 +83,24 @@ void build(int now, int l, int r) {
         int mid = (l + r) >> 1;
         build(now * 2, l, mid);
         build(now * 2 + 1, mid + 1, r);
-        nd[now].col = calc(nd[now * 2].col, nd[now * 2 + 1].col);
+        update(now);
     } else {
-        nd[now].col = s[l];
+        for (int i = 0; i < 8; i++) {
+            nd[now].col[i] = calc(a[i], s[l]);
+        }
+    }
+}
+
+void cover(int now, int ch) {
+    nd[now].flag = ch;
+    for (int i = 0; i < 8; i++) {
+        int x = a[i];
+        for (int j = nd[now].l; j <= nd[now].r; j++) {
+            x = calc(x, ch);
+            if (x == ch)
+                break;
+        }
+        nd[now].col[i] = x;
     }
 }
 
@@ -64,16 +109,15 @@ void pushDown(int now) {
         return;
 
     if (nd[now].l < nd[now].r) {
-        nd[now * 2].flag = nd[now * 2].col = nd[now].flag;
-        nd[now * 2 + 1].flag = nd[now * 2 + 1].col = nd[now].flag;
+        cover(now * 2, nd[now].flag);
+        cover(now * 2 + 1, nd[now].flag);
     }
     nd[now].flag = 0;
 }
 
 void change(int now, int l, int r, char ch) {
     if (l <= nd[now].l && nd[now].r <= r) {
-        nd[now].col = ch;
-        nd[now].flag = ch;
+        cover(now, ch);
         return;
     }
 
@@ -85,31 +129,33 @@ void change(int now, int l, int r, char ch) {
     if (r > mid)
         change(now * 2 + 1, l, r, ch);
 
-    nd[now].col = calc(nd[now * 2].col, nd[now * 2 + 1].col);
+    update(now);
 }
 
-int query(int now, int l, int r) {
+int query(int now, int l, int r, int i) {
     pushDown(now);
     if (l <= nd[now].l && nd[now].r <= r) {
-        return nd[now].col;
+        return nd[now].col[i];
     }
 
     int mid = (nd[now].l + nd[now].r) / 2;
-    int lv = 0, rv = 0;
-    if (l <= mid)
-        lv = query(now * 2, l, r);
-    if (r > mid)
-        rv = query(now * 2 + 1, l, r);
+    int ch = ' ';
+    int idx = i;
+    if (l <= mid) {
+        ch = query(now * 2, l, r, idx);
+        idx = getIdx(ch);
+    }
 
-    if (lv == 0)
-        return rv;
-    else if (rv == 0)
-        return lv;
-    return calc(lv, rv);
+    if (r > mid) {
+        ch = query(now * 2 + 1, l, r, idx);
+    }
+
+    return ch;
 }
 
 int main() {
     scanf("%d", &t);
+
     while (t--) {
         scanf("%d%d", &n, &m);
         scanf("%s", s + 1);
@@ -123,7 +169,7 @@ int main() {
                 change(1, l, r, ch[0]);
             } else {
                 scanf("%d%d", &l, &r);
-                char ans = query(1, l, r);
+                char ans = query(1, l, r, 0);
                 printf("%c\n", ans);
             }
         }
