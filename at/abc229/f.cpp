faa6e1b0f7a0bdade26382008fca18a03c302a19
rocket323 <lennonyang@tencent.com>
2021-11-27 23:24:20 +0800

    update

--- at/abc229/f.cpp
+++ at/abc229/f.cpp
@@ -14,37 +14,48 @@ using ll = long long;
 const int inf = 0x3f3f3f3f;
 const int maxl = 2e5 + 10;
 
-int n, m, a[maxl], b[maxl];
+int n, a[maxl], b[maxl];
 ll f[maxl][2], g[maxl][2];
 
 int main() {
     scanf("%d", &n);
-    m = 0;
     ll sum = 0;
     for (int i = 1; i <= n; i++) {
         scanf("%d", &a[i]);
-        sum += a[i];
+        sum = sum + a[i];
     }
     for (int i = 1; i <= n; i++) {
         scanf("%d", &b[i]);
-        sum += b[i];
+        sum = sum + b[i];
     }
 
-    memset(f, 0, sizeof f);
+    for (int i = 0; i <= n; i++) {
+        f[i][0] = f[i][1] = -inf;
+        g[i][0] = g[i][1] = -inf;
+    }
     f[1][0] = 0;
-    f[1][1] = 0;
-    for (int i = 2; i <= n; i++) {
-        f[i][0] = max({ f[i-1][0] + b[i-1], f[i-1][1] + b[i-1] });
-        f[i][1] = max({ f[i-1][0] + b[i-1] + a[i], f[i-1][1] + a[i] });
-    }
-    ll ans1 = max({ f[n][0] + b[n], f[n][1] + b[n] });
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (f[i][j] < 0)
+                continue;
 
-    memset(g, 0, sizeof g);
-    g[1][0] = 0;
+            for (int k = 0; k < 2; k++) {
+                f[i+1][k] = max(f[i+1][k], f[i][j] + a[i+1] * k + b[i] * (j != k));
+            }
+        }
+    }
+    ll ans1 = max({ f[n][0], f[n][1] + b[n] });
+
     g[1][1] = a[1];
-    for (int i = 2; i <= n; i++) {
-        g[i][0] = max({ g[i-1][0] + b[i-1], g[i-1][1] + b[i-1] });
-        g[i][1] = max({ g[i-1][0] + b[i-1] + a[i], g[i-1][1] + a[i] });
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < 2; j++) {
+            if (g[i][j] < 0)
+                continue;
+
+            for (int k = 0; k < 2; k++) {
+                g[i+1][k] = max(g[i+1][k], g[i][j] + a[i+1] * k + b[i] * (j != k));
+            }
+        }
     }
     ll ans2 = max({ g[n][0] + b[n], g[n][1] });
     cout << sum - max(ans1, ans2) << endl;
