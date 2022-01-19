0d1c467a528de15ce18d0f4e30e98d1c42361681
rocket323 <lennonyang@tencent.com>
2022-01-20 00:12:56 +0800

    update

--- at/abc235/f.cpp
+++ at/abc235/f.cpp
@@ -10,56 +10,61 @@
 using namespace std;
 using ll = long long;
 
-const int inf = 0x3f3f3f3f;
-const int maxl = 1e4 + 10;
-const int mod = 998244353;
+const ll inf = 0x3f3f3f3f;
+const ll maxl = 1e4 + 10;
+const ll mod = 998244353;
 
 char s[maxl];
-int n, m, c[maxl], d[maxl];
-int f[maxl][2][2][1024], g[maxl];
-int vis[maxl];
+ll n, d[maxl];
+int m, c[20];
+int f[maxl][2][2][1024], cnt[maxl][2][2][1024], g[maxl];
+int mask;
 
 int gao() {
     memset(f, -1, sizeof f);
     f[0][0][0][0] = 0;
-    for (int i = 0; i < n; i++)
+    cnt[0][0][0][0] = 1;
+    for (ll i = 0; i < n; i++)
         d[i + 1] = s[i] - '0';
-    int t = (1 << m) - 1;
-    for (int i = 0; i <= n; i++) {
-        for (int q = 0; q < 2; q++) {
-            for (int j = 0; j < 2; j++) {
-                for (int p = 0; p <= t; p++) {
-                    if (f[i][q][j][p] < 0)
+    ll t = 1023;
+    for (ll i = 0; i < n; i++) {
+        for (ll q = 0; q < 2; q++) {
+            for (ll j = 0; j < 2; j++) {
+                for (ll p = 0; p <= t; p++) {
+                    if (cnt[i][q][j][p] == 0)
                         continue;
 
-                    // printf("%d %d %d %d %d\n", i, q, j, p, f[i][q][j][p]);
-                    for (int k = 0; k < 10; k++) {
+                    for (ll k = 0; k < 10; k++) {
                         if (j == 0 && k > d[i + 1])
-                            continue;
+                            break;
 
-                        int ii = i + 1;
-                        int qq = q | (k > 0);
-                        int jj = j | (k < d[i + 1]);
-                        int pp = p;
-                        if (qq && vis[k] >= 0) {
-                            pp = p | (1 << vis[k]);
+                        ll ii = i + 1;
+                        ll qq = q | (k > 0);
+                        ll jj = j | (k < d[i + 1]);
+                        ll pp = p;
+                        if (qq) {
+                            pp = p | (1 << k);
                         }
-                        int x = g[n - ii];
-                        int tmp = (ll)k * x % mod;
+                        ll x = g[n - ii];
+                        ll tmp = (ll)k * x % mod * (ll)cnt[i][q][j][p] % mod;
                         if (f[ii][qq][jj][pp] < 0)
                             f[ii][qq][jj][pp] = 0;
-                        f[ii][qq][jj][pp] = (f[ii][qq][jj][pp] + f[i][q][j][p] + tmp) % mod;
-                        // printf("%d -> %d %d %d %d\n", k, ii, qq, jj, pp);
+                        f[ii][qq][jj][pp] = ((ll)f[ii][qq][jj][pp] + f[i][q][j][p] + tmp) % mod;
+                        cnt[ii][qq][jj][pp] = ((ll)cnt[ii][qq][jj][pp] + cnt[i][q][j][p]) % mod;
                     }
                 }
             }
         }
     }
 
-    int ans = 0;
-    for (int i = 0; i < 2; i++) {
-        if (f[n][1][i][t] > 0)
-            ans = (ans + f[n][1][i][t]) % mod;
+    ll ans = 0;
+    for (ll i = 0; i < 2; i++) {
+        for (ll j = 0; j <= t; j++) {
+            if ((j & mask) != mask)
+                continue;
+            if (f[n][1][i][j] > 0)
+                ans = (ans + f[n][1][i][j]) % mod;
+        }
     }
     return ans;
 }
@@ -68,14 +73,12 @@ int main() {
     scanf("%s", s);
     scanf("%d", &m);
     g[0] = 1;
-    for (int i = 1; i < maxl; i++)
+    for (ll i = 1; i < maxl; i++)
         g[i] = (ll)g[i - 1] * 10 % mod;
     n = strlen(s);
-    for (int i = 0; i < 10; i++)
-        vis[i] = -1;
-    for (int i = 0; i < m; i++) {
+    for (ll i = 0; i < m; i++) {
         scanf("%d", &c[i]);
-        vis[c[i]] = i;
+        mask |= (1 << c[i]);
     }
     printf("%d\n", gao());
     return 0;
