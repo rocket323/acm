c99736f88d71ea75e01ebef329ed3522fbbd626c
rocket323 <lennonyang@tencent.com>
2022-04-04 22:51:47 +0800

    tree dp

--- gcj/2022/qual/d.cpp
+++ gcj/2022/qual/d.cpp
@@ -1,31 +1,38 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+#include <cstring>
+#include <vector>
+#include <iostream>
 using namespace std;
 
 using ll = long long;
+const int inf = 0x3f3f3f3f;
 const int maxl = 1e5 + 10;
 int t, n, a[maxl];
-ll f[maxl];
+ll f[maxl], g[maxl];
 vector<int> adj[maxl];
 
-void dfs2(int root, int u, ll total_sum, int mx) {
+void dfs(int u) {
     ll child_sum = 0;
+    int mg = inf;
     for (int v : adj[u]) {
+        dfs(v);
         child_sum += f[v];
+        if (g[v] < mg)
+            mg = g[v];
     }
-    for (int v : adj[u]) {
-        ll tmp = child_sum - f[v];
-        dfs2(root, v, total_sum + tmp, max(mx, a[v]));
-    }
-    if (adj[u].empty()) {
-        f[root] = max(f[root], total_sum + mx);
-    }
-}
 
-void dfs1(int u) {
-    for (int v : adj[u]) {
-        dfs1(v);
+    g[u] = (mg == inf ? a[u] : max(mg, a[u]));
+    if (adj[u].empty()) {
+        f[u] = a[u];
+    } else {
+        for (int v : adj[u]) {
+            ll tmp = child_sum;
+            if (a[u] > g[v]) {
+                tmp += a[u] - g[v];
+            }
+            f[u] = max(f[u], tmp);
+        }
     }
-    dfs2(u, u, 0, a[u]);
 }
 
 int main() {
@@ -43,7 +50,7 @@ int main() {
             scanf("%d", &u);
             adj[u].push_back(i);
         }
-        dfs1(0);
+        dfs(0);
         cout << "Case #" << tc << ": " << f[0] << endl;
     }
     return 0;
