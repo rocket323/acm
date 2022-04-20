7b032faca6603babd8f5c415709c6a56bb9d4821
lennonyang <lennonyang@tencent.com>
2022-04-20 11:31:43 +0800

    add acm code-snippets

---
+++ cf/478d.cpp
@@ -0,0 +1,17 @@
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <map>
+#include <queue>
+#include <set>
+#include <vector>
+using namespace std;
+using ll = long long;
+
+const int inf = 0x3f3f3f3f;
+const int maxl = 1e5 + 10;
+
+int main() {
+    return 0;
+}
